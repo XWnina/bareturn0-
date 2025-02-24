b13f3875ade1dd1623c6627f95afeb298d5b695a
Nina Wu <wu1765@purdue.edu>
2025-02-24 00:16:36 -0500

    changed the twk to 7days

--- backend_express/src/routes/userRoutes.js
+++ backend_express/src/routes/userRoutes.js
@@ -71,7 +71,7 @@ router.post("/login", async (req, res) => {
         }
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-            expiresIn: "1h",
+            expiresIn: "7d",
         });
 
         const successResponse = { message: "Login successful", token };
