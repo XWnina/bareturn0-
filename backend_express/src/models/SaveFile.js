ffc94b05d79b3403e4af21c9c6de195abfd88b60
Nina Wu <wu1765@purdue.edu>
2025-02-24 22:48:20 -0500

    adding player name into the savefile

--- backend_express/src/models/SaveFile.js
+++ backend_express/src/models/SaveFile.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const SaveFileSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     saveName: { type: String, required: true },
+    playerName: { type: String, default: "|" },
     progress: { type: Number, default: 0 },
     coins: { type: Number, default: 0 },
     timeStamp: { type: Date, default: Date.now },
