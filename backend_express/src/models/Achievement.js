fb38bda0e48e140e689d076c951e575fb4d08e08
Nina Wu <wu1765@purdue.edu>
2025-02-27 22:18:03 -0500

    achievement after printf done

--- backend_express/src/models/Achievement.js
+++ backend_express/src/models/Achievement.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const AchievementSchema = new mongoose.Schema({
-    saveFileId: { type: mongoose.Schema.Types.ObjectId, ref: "SaveFile", required: true },
+    saveFileId: { type: mongoose.Schema.Types.ObjectId, ref: "SaveFile", required: false },
     name: { type: String, required: true }, // Name of the achievement
     method: { type: String, required: true }, // Unlock method
     achievedDate: { type: Date }, // Null if locked
