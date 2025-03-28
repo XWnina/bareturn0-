d8b33763edb32b35fe9d981817c589958eec402a
Nina Wu <wu1765@purdue.edu>
2025-03-28 04:19:23 -0400

    cards

--- backend_express/src/models/SaveFile.js
+++ backend_express/src/models/SaveFile.js
@@ -1,15 +1,29 @@
 const mongoose = require("mongoose");
 
-const SaveFileSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the User model
+const SaveFileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    }, // Reference to the User model
     saveName: { type: String, required: true },
     playerName: { type: String, default: "|" }, // Default player name is "|"
     progress: { type: Number, default: 0 },
     coins: { type: Number, default: 0 },
-    unlockedAchievements: [{
-        achievementId: { type: mongoose.Schema.Types.ObjectId, ref: "Achievement" },
-        achievedDate: { type: Date, default: null }
-    }]  // Reference to the Achievement model
-}, { timestamps: true });
+    unlockedAchievements: [
+      {
+        achievementId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Achievement",
+        },
+        achievedDate: { type: Date, default: null },
+      },
+    ], // Reference to the Achievement model
+    maxHealth: { type: Number, default: 20 },
+    speed: { type: Number, default: 1 }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("SaveFile", SaveFileSchema);
